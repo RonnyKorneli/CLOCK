f2bfeeaf0f8cfdcc45a9e762f8f4541ac9cf1164
Ronny <ronnykorneli@googlemail.com>
2022-08-06 16:20:37 +0200

    Changed from AM/PM to 24Hrs

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
+
 const clock = () => {
 
     let getTime = new Date()
     let hrs = getTime.getHours()
     let min = getTime.getMinutes();
     let sec = getTime.getSeconds();
-
-
-    if (hrs > 12) {
-        document.getElementById("AmPm").innerHTML = "PM"
-    }
-    if (hrs == 0) {
-        hrs = 12;
-        document.getElementById("AmPm").innerHTML = "AM"
-    }
 
     hrs = hrs < 10 ? "0" + hrs : hrs;
     min = min < 10 ? "0" + min : min;
